7f0e44267cdb7180df3df976bcb9edfa049389d7
HangwelaniN <hngwekhulu@gmail.com>
2023-11-04 11:53:15 +0200

    added GitHub repo url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather application</div>;
+  return (
+    <div className="App">
+      <h1>Weather application</h1>
+      <footer>
+        This code was coded by Hangwelani Ngwekhulu and is{" "}
+        <a
+          href="https://github.com/HangwelaniN/weather-appl-react/tree/main/my-app"
+          target="blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
